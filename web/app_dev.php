1b9b9813e5ee1382f053f13f995bd1f45127a937
Jacques <jacques@opo.fr>
2016-10-23 22:49:36 +0200

    wip: docker update

--- web/app_dev.php
+++ web/app_dev.php
@@ -14,7 +14,7 @@ $whiteListedAddresses = ['127.0.0.1', 'fe80::1', '::1'];
 if ($dockerBridgeIp = getenv('DOCKER_BRIDGE_IP')) {
     $whiteListedAddresses[] = $dockerBridgeIp;
 }
-
+/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], $whiteListedAddresses) || php_sapi_name() === 'cli-server')
@@ -22,7 +22,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-
+*/
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
